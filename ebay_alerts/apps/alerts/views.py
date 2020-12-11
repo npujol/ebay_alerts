3eb807cf5153ed6bbaa597ba085209eff2234508
npujol <naivy.luna@gmail.com>
2020-12-11 16:56:00 +0100

    Add view

--- ebay_alerts/apps/alerts/views.py
+++ ebay_alerts/apps/alerts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from rest_framework.permissions import AllowAny
+from .models import Alert, Account
+from .serializers import AlertSerializer
+
+
+class AlertViewSet(viewsets.ModelViewSet):
+    """
+    General ViewSet description
+
+    list: List the alerts
+
+    retrieve: Retrieve an alert
+
+    update: Update an alert
+
+    create: Create an alert
+
+    partial_update: Patch an alert
+
+    destroy: Delete an alert
+
+    """
+
+    permission_classes = (AllowAny,)
+    serializer_class = AlertSerializer
+
+    queryset = Alert.objects.select_related("owner", "owner__email")
+
+    def list(self, request, *args, **kwargs):
+        owner = self.request.query_params.get("owner", None)
+
+        if owner is not None:
+            self.queryset = self.queryset.filter(owner__email=owner)
+        return super().list(self, request, *args, **kwargs)
+
+
+    
