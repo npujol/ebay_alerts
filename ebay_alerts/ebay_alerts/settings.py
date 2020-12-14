8cc7af49b772e3bdff0eabbf0c2f84c9079900de
npujol <naivy.luna@gmail.com>
2020-12-14 14:45:26 +0100

    Setup celery

--- ebay_alerts/ebay_alerts/settings.py
+++ ebay_alerts/ebay_alerts/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "drf_yasg",
-    
     "apps.alerts.apps.AlertsConfig",
 ]
 
@@ -147,3 +146,9 @@ REST_FRAMEWORK = {
 }
 
 SWAGGER_SETTINGS = {"USE_SESSION_AUTH": False}
+
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TASK_SERIALIZER = "json"
