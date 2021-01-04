9b225f0298e3fd43a495cb10924a8cbf6dd83f57
npujol <naivy.luna@gmail.com>
2021-01-04 12:55:13 +0100

    Update filters to get the data from ebay

--- ebay_alerts/apps/alerts/ebay_data.py
+++ ebay_alerts/apps/alerts/ebay_data.py
@@ -1,10 +1,9 @@
 import datetime
 from django.conf import settings
 from ebaysdk.finding import Connection
-from .fake_ebay import get_filtered_data
 
 
-def get_ebay_data(search_term, limit=10, interval_time=30):
+def get_ebay_data(search_term, limit=10, interval_time=30, only_new=False):
     try:
         api = Connection(
             domain="svcs.sandbox.ebay.com",
@@ -13,16 +12,25 @@ def get_ebay_data(search_term, limit=10, interval_time=30):
         )
         now = datetime.datetime.utcnow()
         now_minus_interval = now - datetime.timedelta(minutes=int(interval_time))
-        print(f"fecha {str(now_minus_interval)}")
-        request = {
-            "keywords": search_term,
-            "itemFilter": [
-                {"name": "condition", "value": "new"},
-                # {"name": "StartTimeFrom", "value": now_minus_interval.isoformat()},
-            ],
-            "paginationInput": {"entriesPerPage": limit, "pageNumber": 1},
-            "sortOrder": "PricePlusShippingLowest",
-        }
+        if only_new:
+            request = {
+                "keywords": search_term,
+                "itemFilter": [
+                    {"name": "condition", "value": "new"},
+                    {"name": "StartTimeFrom", "value": now_minus_interval.isoformat()},
+                ],
+                "paginationInput": {"entriesPerPage": limit, "pageNumber": 1},
+                "sortOrder": "PricePlusShippingLowest",
+            }
+        else: 
+            request = {
+                "keywords": search_term,
+                "itemFilter": [
+                    {"name": "condition", "value": "new"},
+                ],
+                "paginationInput": {"entriesPerPage": limit, "pageNumber": 1},
+                "sortOrder": "PricePlusShippingLowest",
+            }
         response = api.execute("findItemsAdvanced", request)
     except Exception:
         return []
@@ -31,7 +39,3 @@ def get_ebay_data(search_term, limit=10, interval_time=30):
             if response.reply.searchResult.item:
                 return response.reply.searchResult.item
     return []
-
-
-def get_ebay_data_fake(search_term):
-    return get_filtered_data(search_term, limit=2)
