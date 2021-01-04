a93c5c31b3b69824ebd18759e8e8b4884a643509
npujol <naivy.luna@gmail.com>
2021-01-04 12:25:49 +0100

    Add abstract class with the common fields

--- ebay_alerts/apps/alerts/models.py
+++ ebay_alerts/apps/alerts/models.py
@@ -1,44 +1,37 @@
+
 import uuid
 from django.db import models
-from django.db.models import F, Case, Value, ExpressionWrapper, fields, When
-from datetime import datetime, timedelta
 
-
-class Account(models.Model):
+class BaseModel(models.Model):
     uuid = models.UUIDField(
         primary_key=True, unique=True, default=uuid.uuid4, editable=False
-    )
-    email = models.EmailField(
-        unique=True,
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        def __str__(self):
-            return self.email
+        abstract = True
+
+class Account(BaseModel): 
+    email = models.EmailField(
+        unique=True,
+    )
+   
+    def __str__(self):
+        return self.email
 
 
 class AlertQuerySet(models.QuerySet):
-    def email_every_two_minutes(self):
-        return self.filter(interval_time=2)
+    def email_every_minutes(self, interval_time):
+        return self.filter(interval_time=interval_time)
+    
 
-    def email_every_ten_minutes(self):
-        return self.filter(interval_time=10)
-
-    def email_every_thirty_minutes(self):
-        return self.filter(interval_time=30)
-
-
-class Alert(models.Model):
+class Alert(BaseModel):
     class IntervalOfTime(models.TextChoices):
         TWO = 2, "2 minutes"
         TEN = 10, "10 minutes"
         THIRTY = 30, "30 minutes"
 
-    uuid = models.UUIDField(
-        primary_key=True, unique=True, default=uuid.uuid4, editable=False
-    )
     owner = models.ForeignKey(
         Account,
         on_delete=models.CASCADE,
@@ -50,8 +43,6 @@ class Alert(models.Model):
         choices=IntervalOfTime.choices,
         default=IntervalOfTime.THIRTY,
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     objects = AlertQuerySet.as_manager()
 
@@ -59,5 +50,5 @@ class Alert(models.Model):
         unique_together = [["search_term", "interval_time", "owner"]]
         ordering = ["updated_at"]
 
-        def __str__(self):
-            return f"Alert number: {self.id}"
+    def __str__(self):
+        return f"Alert number: {self.id}"
