844624979a268a43889945f35f868f9e333f7db6
npujol <naivy.luna@gmail.com>
2021-01-07 15:44:35 +0100

    Add base to heroku

--- ebay_alerts/ebay_alerts/settings/base.py
+++ ebay_alerts/ebay_alerts/settings/base.py
@@ -28,7 +28,7 @@ DEBUG = env("DEBUG")
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 DEFAULT_BASE_URL = "http://localhost:8000"
 
